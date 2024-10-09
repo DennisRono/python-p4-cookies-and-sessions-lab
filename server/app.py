19cdc1608c236871c2a9eb77fd23b2c79e0cfaa0
Dennis Kibet <dennisrkibet@gmail.com>
2024-10-09 14:59:35 +0300

    cookies set ans session blog app

--- server/app.py
+++ server/app.py
@@ -1,34 +1,62 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify, session
+from flask import Flask, jsonify, session
 from flask_migrate import Migrate
-
 from models import db, Article, User
 
 app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.secret_key = b"Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
-
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
-@app.route('/clear')
+
+@app.route("/clear")
 def clear_session():
-    session['page_views'] = 0
-    return {'message': '200: Successfully cleared session data.'}, 200
+    session["page_views"] = 0
+    return {"message": "200: Successfully cleared session data."}, 200
 
-@app.route('/articles')
+
+@app.route("/articles")
 def index_articles():
+    articles = Article.query.all()
+    articles_list = [
+        {"id": article.id, "title": article.title, "content": article.content}
+        for article in articles
+    ]
+    return jsonify(articles_list), 200
 
-    pass
 
-@app.route('/articles/<int:id>')
+@app.route("/articles/<int:id>")
 def show_article(id):
 
-    pass
+    session["page_views"] = session.get("page_views", 0) + 1
 
-if __name__ == '__main__':
+    if session["page_views"] > 3:
+        return {"message": "Maximum pageview limit reached"}, 401
+
+    article = Article.query.get(id)
+    if article:
+        return (
+            jsonify(
+                {
+                    "id": article.id,
+                    "author": article.author,
+                    "title": article.title,
+                    "content": article.content,
+                    "preview": article.preview,
+                    "minutes_to_read": article.minutes_to_read,
+                    "date": article.date,
+                }
+            ),
+            200,
+        )
+    else:
+        return {"message": "Article not found"}, 404
+
+
+if __name__ == "__main__":
     app.run(port=5555)
